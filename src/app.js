1f745d2675197fb637469ee6b985bf25cd46dedb
YVES <100066447+iraduyves@users.noreply.github.com>
2024-12-13 17:25:22 +0200

    fix:fix bug

--- src/app.js
+++ src/app.js
@@ -61,7 +61,6 @@ app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-app.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 
 app.get('/', (req, res) => {
